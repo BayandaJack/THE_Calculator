65f00dd00a53171a833a4c649ce2578a46b75886
Bayanda Jack <bayanda45jack@gmail.com>
2025-06-21 14:21:38 +0200

    The numbers now show on the display part when clicked and AC clears all

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
+  const buttons = document.querySelectorAll(".calc-btn");
+  const display = document.querySelector(".display-calc");
+  //Loop through the buttons and add click functionality
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const value = button.textContent;
+      if (value === "AC"){
+        display.textContent = "";
+      }else{
+        display.textContent = display.textContent + value;
+        //console.log('Button clicked: ', value);
+      }
+    })
+  })
 
 });
