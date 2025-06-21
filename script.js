e504106fda6d51c151ba409fac7ccbb8caec4d70
Bayanda Jack <bayanda45jack@gmail.com>
2025-06-21 14:37:15 +0200

    Added a sum variable to add up the stuff

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   // Your code here
   const buttons = document.querySelectorAll(".calc-btn");
   const display = document.querySelector(".display-calc");
+  let sum = 0;
   //Loop through the buttons and add click functionality
   buttons.forEach(button => {
     button.addEventListener('click', () => {
       const value = button.textContent;
       if (value === "AC"){
         display.textContent = "";
-      }else{
+      }else if (typeof value === 'number'){
         display.textContent = display.textContent + value;
+        sum = sum + parseInt(value);
         //console.log('Button clicked: ', value);
       }
     })
